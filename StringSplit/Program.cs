51ac806aba7998c74e204ad206c1c88a6d1d72d9
SaymanDotNet <saymannsk@gmail.com>
2025-03-13 11:34:11 +0700

    Добавьте файлы проекта.

---
+++ StringSplit/Program.cs
@@ -0,0 +1,8 @@
+﻿string words = "Дана строка с  текстом, используя метод строки String.Split() получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
+
+string[]  arrayWords = words.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (string word in arrayWords)
+{
+	Console.WriteLine(word);
+}
